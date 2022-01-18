d9fdcac63f9940bab01de10729a4d761ff6378a2
Anastasiia Svetlova <anastasiiasvietlova@gmail.com>
2022-01-18 21:32:04 +0200

    added slider

--- scripts/index.js
+++ scripts/index.js
@@ -5,11 +5,17 @@ const curency_mapping = {
     "EUR": "€",
 }
 
+const DEFAULT_NO_IMG = "./img/no-image.jpg"
+
 function curencyExchange(price, currencyType) {
+ let roundPrice = (number) => {
+        return +number.toFixed(2);
+    }
+
     if (currencyType === CURRENCY) {
         return price;
     }
-    return price * CURRENCY_EXCHANGE[currencyType];
+    return roundPrice(price * CURRENCY_EXCHANGE[currencyType]);
 }
 
 const curencySymbol = curency_mapping[CURRENCY];
@@ -21,54 +27,164 @@ for (let item of elements) {
 }
 
 
-// const typeToClassMapping = {
-//     "new": "new-items-slider-label",
-//     "recommended": "new-items-slider-label-like",
-//     "sale": "new-items-slider-label-percent",
-// }
+const NewItems = []
+const RecommendedItems = []
+const SaleItems = []
 
-// let startItemIdx = 0;
+for (let item of ITEMS) {
+    switch (item.type) {
+        case "new":
+            NewItems.push(item);
+            break;
 
-// function updateNewItems() {
-//     let currentInx = startItemIdx;
-//     let newItemsHTML = document.querySelectorAll(".new-items-slider .slider-item");
-//     for (let itemHTML of newItemsHTML) {
-//         if (currentInx < ITEMS.length) {
-            
-//             let itemJS = ITEMS[currentInx];
-//             if (itemJS.type !== "new") {
-//                currentInx++;
-//                continue
-//            }
+        case "sale":
+            SaleItems.push(item);
+            break;
+
+        case "recommended":
+            RecommendedItems.push(item);
+            break;
+
+        default:
+            console.log("Unknown item type", item.type);
+            break;
+    }
+}
+
+console.log("NewItems lenght=", NewItems.length);
+
+console.log("RecommendedItems lenght=", RecommendedItems.length);
+
+console.log("SaleItems lenght=", SaleItems.length);
+
+
+// TODO: change to 0 
+let startNewItemIdx = 1;
+let startRecomendedItemIdx = 1;
+let startSaleItemIdx = 1;
+
+
+function updateItems(itemsHTML, itemsArray, startIdx) {
+    let currentInx = startIdx;
+
+    for (let itemHTML of itemsHTML) {
+        if (currentInx < itemsArray.length) {
+            let itemJS = itemsArray[currentInx];
     
-//             let imgItemHTML = itemHTML.querySelector(".new-items-slider-img");
-//             imgItemHTML.src = itemJS.img;
+            let imgItemHTML = itemHTML.querySelector(".new-items-slider-img");
+            if (itemJS.img != undefined && itemJS.img != "") {
+                imgItemHTML.src = itemJS.img;
+            } else { 
+                imgItemHTML.src = DEFAULT_NO_IMG;
+            }
            
-//             let descriptionHTML = itemHTML.querySelector(".new-items-slider-name");
-//             descriptionHTML.innerText = itemJS.description;
+            let descriptionHTML = itemHTML.querySelector(".new-items-slider-name");
+            descriptionHTML.innerText = itemJS.description;
+            // 
+            if (itemJS.url != undefined && itemJS.url !== "") {
+                descriptionHTML.href = itemJS.url;
+            } else {
+                descriptionHTML.href = "#";
+            }
            
-//             let currPriceHTML = itemHTML.querySelector(".current-price");
-//             currPriceHTML.innerText = curencyExchange(itemJS.price, itemJS.currency);
+            let currPriceHTML = itemHTML.querySelector(".current-price");
+            currPriceHTML.innerText = curencyExchange(itemJS.price, itemJS.currency);
     
-//             currentInx++;
-//         }
-//     }
-// }
+            let oldPrice = itemHTML.querySelector(".old-price-holder .previous-price");
+            let oldPriceCurrency = itemHTML.querySelector(".old-price-holder .currency-symbol");
 
-// let navRowLeft = document.querySelector(".new-items-slider img:first-child");
-// navRowLeft.onclick = () => {
-//     if (startItemIdx > 0) {
-//         startItemIdx--;
-//         updateNewItems();
-//     }
-// }
+            if (itemJS.oldPrice != undefined && itemJS.oldPrice != "") {
+                oldPrice.innerText = curencyExchange(itemJS.oldPrice, itemJS.currency);
+                oldPriceCurrency.innerText = curencySymbol;
+            } else {
+                oldPrice.innerText = ""
+                oldPriceCurrency.innerText = ""
+            }
+            currentInx++;
+        }
+    }
+}
 
-// let navRowLeft = document.querySelector(".new-items-slider img:last-child");
-// navRowLeft.onclick = () => {
-//     let maxCurrentLenght =  4;
-//     if (startItemIdx + maxCurrentLenght + 1 < ITEMS.length ) {
-//         startItemIdx++;
-//         updateNewItems();
-//     }
-// }
-// // new-items-slider 
+// TODO:
+// 
+let newItemsHTML = document.querySelectorAll(".new-items-slider .slider-item");
+updateItems(newItemsHTML, NewItems, startNewItemIdx); // Initial initialization
+
+
+let recomendedItemsHTML = document.querySelectorAll(".recommended-items-slider .slider-item");
+updateItems(recomendedItemsHTML, RecommendedItems, startRecomendedItemIdx); // Initial initialization
+
+
+let saleItemsHTML = document.querySelectorAll(".sale-items-slider .slider-item");
+updateItems(saleItemsHTML, SaleItems, startSaleItemIdx); // Initial initialization
+// 
+
+
+
+//// NEW ITEMS /////
+document.querySelector(".new-items-slider .left-button-slider").onclick = () => {
+    if (startNewItemIdx > 0) {
+        startNewItemIdx--;
+        let newItemsHTML = document.querySelectorAll(".new-items-slider .slider-item");
+        updateItems(newItemsHTML, NewItems, startNewItemIdx);
+    } else {
+       // this.src =  ""
+    }
+}
+
+document.querySelector(".new-items-slider .right-button-slider").onclick = () => {
+    let maxCurrentLenght =  document.querySelectorAll('.new-items-slider .slider-item:not([style="display: none"])').length;
+
+    if (startNewItemIdx + maxCurrentLenght < NewItems.length ) {
+        startNewItemIdx++;
+        let newItemsHTML = document.querySelectorAll(".new-items-slider .slider-item");
+        updateItems(newItemsHTML,NewItems, startNewItemIdx);
+    } else {
+
+    }
+}
+// ////////////////////////////////
+
+document.querySelector(".recommended-items-slider .left-button-slider").onclick = () => {
+    if (startRecomendedItemIdx > 0) {
+        startRecomendedItemIdx--;
+        let recomendedItemsHTML = document.querySelectorAll(".recommended-items-slider .slider-item");
+        updateItems(recomendedItemsHTML, RecommendedItems, startRecomendedItemIdx);
+    } else {
+       // this.src =  ""
+    }
+}
+
+document.querySelector(".recommended-items-slider .right-button-slider").onclick = () => {
+    let maxCurrentLenght =  document.querySelectorAll('.recommended-items-slider .slider-item:not([style="display: none"])').length;
+
+    if (startRecomendedItemIdx + maxCurrentLenght < RecommendedItems.length ) {
+        startRecomendedItemIdx++;
+        let RecommendedItemsHTML = document.querySelectorAll(".recommended-items-slider .slider-item");
+        updateItems(RecommendedItemsHTML,RecommendedItems, startRecomendedItemIdx);
+    } else {
+
+    }
+}
+
+document.querySelector(".sale-items-slider .left-button-slider").onclick = () => {
+    if (startSaleItemIdx > 0) {
+        startSaleItemIdx--;
+        let saleItemsHTML = document.querySelectorAll(".sale-items-slider .slider-item");
+        updateItems(saleItemsHTML, SaleItems, startSaleItemIdx);
+    } else {
+       // this.src =  ""
+    }
+}
+
+document.querySelector(".sale-items-slider .right-button-slider").onclick = () => {
+    let maxCurrentLenght =  document.querySelectorAll('.sale-items-slider .slider-item:not([style="display: none"])').length;
+
+    if (startSaleItemIdx + maxCurrentLenght < SaleItems.length ) {
+        startSaleItemIdx++;
+        let SaleItemsHTML = document.querySelectorAll(".sale-items-slider .slider-item");
+        updateItems(SaleItemsHTML,SaleItems, startSaleItemIdx);
+    } else {
+
+    }
+}
